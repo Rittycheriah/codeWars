ef9c77b43cdac0bfba3870262934aa8294f329b2
rittycheriah <rittycheriah@gmail.com>
2015-11-17 21:57:09 -0600

    showing how ES6 changed JS class extends

---
+++ prototypingExtendsClass.js
@@ -0,0 +1,14 @@
+lass Animal { 
+  constructor(name) {
+    this.name = name;
+  }
+  
+  speak() {
+    return this.name + ' makes a noise, ';
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    return this.name + ' makes a noise, ' + this.name + ' goes meow.';
+  }
