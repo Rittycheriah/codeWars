f44ceaf0795302029823f42ea744fad5ba210914
rittycheriah <rittycheriah@gmail.com>
2015-11-17 22:13:38 -0600

    Kata showing how to link the input to a position in an array

---
+++ arrayInputReturn.js
@@ -0,0 +1,6 @@
+function countArara(n) {
+  var arara = ["anane", "adak", "adak anane", "adak adak", "adak adak anane", "adak adak adak", "adak adak adak anane", "adak adak adak adak"]
+  var positionToReturn = n - 1;
+  
+  return arara[positionToReturn];
+}
